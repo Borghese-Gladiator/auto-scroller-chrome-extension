76e47e40b1faa677a0c91a2d911acc5ee9da371d
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-05-01 13:23:17 -0400

    outlined UI of popup

--- popup.js
+++ popup.js
@@ -1,46 +1,24 @@
 document.addEventListener('DOMContentLoaded', function () {
-  console.log("LOADING");
-  const nextPageButton = document.getElementById('nextPageButton');
-  const showSnackbarButton = document.getElementById('showSnackbar');
-  nextPageButton.addEventListener('click', function () {
-    chrome.tabs.update({
-      url: "https://www.sitepoint.com/create-chrome-extension-10-minutes-flat/"
-    })
-    window.scrollTo(0, 300)
+  const startBtn = document.getElementById('startBtn');
+  startBtn.addEventListener('click', function ping() {
+    chrome.runtime.sendMessage('startScroll', response => {
+      // Do whatever you want, background script is ready now
+      console.log(response)
+      console.log("BLAH")
+    });
   });
-  showSnackbarButton.addEventListener('click', function myFunction() {
-    var x = document.getElementById("snackbar");
-    x.className = "show";
-    setTimeout(function () { x.className = x.className.replace("show", ""); }, 3000);
-  })
-
-}, false);
-/*
-{
-  "manifest_version": 2,
-  "name": "GTmetrix Analyzer Plugin",
-  "description": "This extension will analyze a page using GTmetrix",
-  "version": "1.0",
-  "browser_action": {
-    "default_icon": {
-      "16": "images/get_started16.png",
-      "32": "images/get_started32.png",
-      "48": "images/get_started48.png",
-      "128": "images/get_started128.png"
-    },
-    "default_popup": "popup.html"
-  },
-  "permissions": [
-    "activeTab",
-    "tabs"
-  ],
-  "icons": {
-    "16": "images/get_started16.png",
-    "32": "images/get_started32.png",
-    "48": "images/get_started48.png",
-    "128": "images/get_started128.png"
+  // display scroll interval slider value
+  const intervalInputElem = document.getElementById("scrollIntervalRange");
+  const intervalSpanElem = document.getElementById("scrollIntervalLabel");
+  intervalSpanElem.innerHTML = intervalInputElem.value;
+  intervalInputElem.oninput = function () {
+    intervalSpanElem.innerHTML = this.value;
   }
-}
-
-
-*/
+  // display scroll distance slider value
+  const distanceInputElem = document.getElementById("scrollDistanceRange");
+  const distanceSpanElem = document.getElementById("scrollDistanceLabel");
+  distanceSpanElem.innerHTML = distanceInputElem.value;
+  distanceInputElem.oninput = function () {
+    distanceSpanElem.innerHTML = this.value;
+  }
+}, false);
